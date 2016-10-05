8ada98288341abb93b9ff8f1d7f0bb4a7f49f460
Michael Peterson <mrpeters@stanford.edu>
2016-10-04 19:39:22 -0700

    Setting up graphical interface for creation

---
+++ components/create/canvas.js
@@ -0,0 +1,9 @@
+function setup() {
+  var canvas = createCanvas(100, 100);
+  console.log("Word");
+  // Move the canvas so it's inside our <div id="sketch-holder">.
+  canvas.parent('sketch-holder');
+
+  background(255, 0, 200);
+}
+
---
+++ components/create/createController.js
@@ -0,0 +1,5 @@
+'use strict';
+
+soccerDraw.controller('CreateController', ['$scope', '$http', '$resource', '$location', '$rootScope',
+  function ($scope, $http, $resource, $location, $rootScope) {
+  }]);
