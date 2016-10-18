2fb261c900610e89235d9dcb3b8faee239c837b3
Michael Peterson <mrpeters@stanford.edu>
2016-10-17 23:19:23 -0700

    Working implementation of adding players for both teams and dragging them around screen

--- components/create/createController.js
+++ components/create/createController.js
@@ -1,12 +1,39 @@
-//'use strict';
+
 
 soccerDraw.factory('play-creation', ['p5', function(p5) {
 	return function (sketch) {
+		players = [];
+		mousePressStartX = -100;
+		mousePressStartY = -100;
+		mouseCurrentlyPressed = false;
 
 		sketch.setup = function () {
 			sketch.createCanvas(1200, 500);
+			sketch.frameRate(60);
+			drawInitialField();
+
+			addYourPlayerButton = sketch.createButton('Add Your Player');
+			addYourPlayerButton.position(10, 550);
+			addYourPlayerButton.mousePressed(addYourPlayer);
+
+			addOpposingPlayerButton = sketch.createButton('Add Opposing Player');
+			addOpposingPlayerButton.position(20 + addYourPlayerButton.width, 550);
+			addOpposingPlayerButton.mousePressed(addOpposingPlayer);
+
+
+			function addYourPlayer() {
+				var player = new Player(true);
+				players.push(player);
+			}
+
+			function addOpposingPlayer() {
+				var player = new Player(false);
+				players.push(player);
+			}
+		}
+
+		function drawInitialField() {
 			sketch.background("green");
-			//sketch.line(0, 0, width, height);
 			sketch.stroke(0, 0, 0);
 			sketch.strokeWeight(4);
 
@@ -36,16 +63,87 @@ soccerDraw.factory('play-creation', ['p5', function(p5) {
 			sketch.noFill();
 			sketch.arc(600, 194.6, 324.4, 324.4, 0.65, 2.48);
 
+			//Corner flags
+			sketch.stroke(0, 0, 0);
+			sketch.strokeWeight(2);
+			sketch.arc(0, 0, 30, 30, 0, 1.57);
+			sketch.arc(1200, 0, 30, 30, 1.57, 3.14);
+
+		}
+
+		function Player(yourTeam) {
+			this.x = 200;
+			this.y = 200;
+			this.radius = 12;
+			this.moving = false;
+			this.yourTeam = yourTeam;
+
+			this.setMovement = function(movement) {
+				this.moving = movement;
+			}
+
+			this.isMoving = function() {
+				return this.moving;
+			}
+
+			this.move = function(newX, newY) {
+				this.x = newX;
+				this.y = newY;
+				this.display();
+			}
+
+			this.display = function() {
+				if (this.yourTeam) {
+					sketch.stroke(0, 0, 0);
+					sketch.fill(189, 32, 49);
+				} else {
+					sketch.stroke(0, 0, 0);
+					sketch.fill(0, 0, 0);
+				}
+				sketch.ellipse(this.x, this.y, this.radius, this.radius);
+			}
+
+			this.shouldMove = function() {
+				return (Math.abs(mousePressStartX - this.x) <= this.radius && Math.abs(mousePressStartY - this.y) <= this.radius)
+			}
+		}
+
+		function setPressStartPoint() {
+			mousePressStartX = sketch.mouseX;
+			mousePressStartY = sketch.mouseY;
+			mouseCurrentlyPressed = true;
 		}
 
 		sketch.draw = function () {
-		}
+			drawInitialField();
+			moved = false;
+			for (var x = 0; x < players.length; x++) {
+				var player = players[x];
 
+				if (mouseCurrentlyPressed === false && sketch.mouseIsPressed) {
+					setPressStartPoint();
+				}
+				if (sketch.mouseIsPressed && player.isMoving() === false && player.shouldMove()) {
+					player.setMovement(true);
+				} 
+				if (player.isMoving()) {
+					player.move(sketch.mouseX, sketch.mouseY);
+				} 
+				if (sketch.mouseIsPressed === false) {
+					mouseCurrentlyPressed = false;
+					if (player.isMoving()) player.setMovement(false);
+				} 
+				
+				player.display();
+			}
+		}
 	};
+
+
 }]);
 
 soccerDraw.controller('CreateController', ['$scope', '$http', '$resource', '$location', '$rootScope', 'p5',
   function ($scope, $http, $resource, $location, $rootScope, p5) {
-  	//console.log(document.getElementById('play-creation'));
+  	var sketch = document.getElementById('play-creation');
 }]);
 
