d31a4352814ca5f4d15f2c5ad7d396628eac823b
Michael Peterson <mrpeters@stanford.edu>
2016-12-08 10:34:24 -0800

    Web server request body length increased

--- webServer.js
+++ webServer.js
@@ -5,12 +5,12 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var async = require('async');
 
-
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname));
 app.use(session({secret: 'secretKey', resave: false, saveUninitialized: false}));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 /* Setup postgres DB. */
 pg.defaults.ssl = true;
