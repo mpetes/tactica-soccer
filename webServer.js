236b1e7d7e3d0d9e7031e74933cb099e58ce9e75
Michael Peterson <mrpeters@stanford.edu>
2016-12-08 22:57:05 -0800

    bug fix

--- webServer.js
+++ webServer.js
@@ -9,7 +9,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname));
 app.use(session({secret: 'secretKey', resave: false, saveUninitialized: false}));
-app.use(bodyParser.json({limit: '50mb'});
+app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 /* Setup postgres DB. */
